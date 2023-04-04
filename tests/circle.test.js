409ca380f90f8db4ce9a498954c1806e1bd324ce
xybai0103 <xueyin0103@gmail.com>
2023-04-04 15:06:22 -0500

    modify Shape class and Circle class, creat a testing suite for the Circle class, test and pass

---
+++ tests/circle.test.js
@@ -0,0 +1,11 @@
+// created a testing suite for the Circle class to verify that the string 
+// for the corresponding SVG file with the given shape colors renders correctly.
+const Circle = require('../lib/circle.js');
+
+describe('Circle', () => {
+  test('should render the string for a SVG file with a circle and the given shape color', () => {
+    const shape = new Circle();
+    shape.color = 'green';
+    expect(shape.render()).toEqual('<circle cx="150" cy="105" r="80" fill="green" />');
+  });
+});
