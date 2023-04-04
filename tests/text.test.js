ff07db29580aba3df47a4fb92d6c381b32efee88
xybai0103 <xueyin0103@gmail.com>
2023-04-04 15:31:20 -0500

    create a testing suite for the Text class. Test and pass

---
+++ tests/text.test.js
@@ -0,0 +1,12 @@
+// created a testing suite for the Text class to verify that the string 
+// for the corresponding SVG file with the given text colors renders correctly.
+const Text = require('../lib/text.js');
+
+describe('Text', () => {
+  test('should render the string for a SVG file with the text and the given text color', () => {
+    const newText = new Text();
+    newText.text = 'OOP';
+    newText.color = 'white';
+    expect(newText.render()).toEqual('<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">OOP</text>');
+  });
+});
