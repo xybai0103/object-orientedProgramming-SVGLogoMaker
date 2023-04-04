af8e50e4342442d35053c24e3b0027c63cbef7e4
xybai0103 <xueyin0103@gmail.com>
2023-04-04 15:17:28 -0500

    modify the Triangle class. Creat a testing suite for the Triangle class. Test and pass

---
+++ tests/triangle.test.js
@@ -0,0 +1,11 @@
+// created a testing suite for the Triangle class to verify that the string 
+// for the corresponding SVG file with the given shape colors renders correctly.
+const Triangle = require('../lib/triangle.js');
+
+describe('Triangle', () => {
+  test('should render the string for a SVG file with a triangle and the given shape color', () => {
+    const shape = new Triangle();
+    shape.color = 'red';
+    expect(shape.render()).toEqual('<polygon points="150, 0 264, 182 36, 182" fill="red" />');
+  });
+});
