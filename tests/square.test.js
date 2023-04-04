44bf3532edfb85984c05b94025248d9d47d83e28
xybai0103 <xueyin0103@gmail.com>
2023-04-04 15:12:05 -0500

    modify the Square class. Create a testing suite for the Square class. Test and pass

---
+++ tests/square.test.js
@@ -0,0 +1,11 @@
+// created a testing suite for the Square class to verify that the string 
+// for the corresponding SVG file with the given shape colors renders correctly.
+const Square = require('../lib/square.js');
+
+describe('Square', () => {
+  test('should render the string for a SVG file with a square and the given shape color', () => {
+    const shape = new Square();
+    shape.color = 'blue';
+    expect(shape.render()).toEqual('<rect x="48" y="10" width="200" height="200" fill="blue" />');
+  });
+});
